c9ade093295a9d380f6d677f3413339e257c6f24
DaggerAlmanza <85371943+DaggerAlmanza@users.noreply.github.com>
2021-09-16 17:36:46 -0500

    sum_consumptions creación

--- app/app.py
+++ app/app.py
@@ -7,6 +7,9 @@ app = Flask(__name__)
 @app.route('/electrodomesticos', methods=['POST'])
 def electrodomesticos():
     data = request.get_json()
-    iron = HomeAppliance(**data["homeAppliaces"][0])
-    
-    return jsonify({'datos': data})
+    for iter_number in range(len(data["homeAppliaces"])):
+        household_appliance = HomeAppliance(
+                            **data["homeAppliaces"][iter_number])
+        sum_consumptions = household_appliance.get_consume()
+
+    return jsonify({'Consumo Total': sum_consumptions})
