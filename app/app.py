5007f83ad779f5b3c2ef879bf0ac07cef69cf4c5
DaggerAlmanza <85371943+DaggerAlmanza@users.noreply.github.com>
2021-09-30 20:18:04 -0500

    Corrección frontEnd

--- app/app.py
+++ app/app.py
@@ -1,24 +1,22 @@
-from flask import Flask, request, jsonify, render_template, url_for
+from flask import Flask, request, render_template, url_for, Response
 from core.models import HomeAppliance
-
+import json
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def portada():
-    return render_template("datos.html")
+    return render_template("datos.html", sum_consumptions=0)
 
 
 @app.route('/electrodomesticos', methods=['POST'])
 def electrodomesticos():
     if request.method == "POST":
         home_appliances = request.get_json()
-        print(home_appliances)
         sum_consumptions: float = 0
         for home_appliance in home_appliances["homeAppliances"]:
             household_appliance = HomeAppliance(**home_appliance)
             sum_consumptions += household_appliance.get_consume()
-        print("valor ", sum_consumptions)
-        return render_template("datos.html", sum_consumptions=sum_consumptions)
-# jsonify({'Consumo Total': sum_consumptions})
+    return Response(json.dumps({"consumptions": sum_consumptions}), mimetype="application/json") 
+# jsonify({'Consumo Total': sum_consumptions}) jsonify,
