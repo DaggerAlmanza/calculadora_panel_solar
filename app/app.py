9834df65090828ec26ce715346d99d57106f50c3
DaggerAlmanza <85371943+DaggerAlmanza@users.noreply.github.com>
2021-10-02 16:34:16 -0500

    Funcionalidades de la  App

--- app/app.py
+++ app/app.py
@@ -1,6 +1,8 @@
+import json
+import math
 from flask import Flask, request, render_template, url_for, Response
 from core.models import HomeAppliance
-import json
+
 
 app = Flask(__name__)
 
@@ -29,19 +31,36 @@ def home_appliances() -> json:
         ]	
     }
     , procesamos la información y devolvemos un json.
+    sum_panel: cantidad de paneles solares
 
     """
     if request.method == "POST":
         home_appliances = request.get_json()
         sum_consumptions: float = 0
         sum_panel: float = 0
+        intensity_daily: float = 0
+        panels_watt: float = 0
+        value_panels = int(home_appliances["solarPanel"])
         for home_appliance in home_appliances["homeAppliances"]:
             household_appliance = HomeAppliance(**home_appliance)
             sum_consumptions += household_appliance.get_consume()
-        sum_panel = (sum_consumptions*1.3)/(
-                                    int(home_appliances["solarPanel"])*4)
-        print(sum_panel)
+
+        sum_panel = (sum_consumptions*1.3)/(value_panels*4)
+        decimal_part, all_in_one_parte = math.modf(sum_panel)
+
+        if decimal_part >= 0.5:
+            sum_panel = all_in_one_parte + 1
+        else:
+            sum_panel = all_in_one_parte
+
+        intensity_daily = (sum_consumptions/48)
+        battery_bank_current = (2*intensity_daily)/0.7
+        print(intensity_daily, battery_bank_current)
+
+        panels_watt = sum_panel*value_panels
+        print(panels_watt)
+
     return Response(
-        json.dumps({"consumptions": sum_consumptions}),
+        json.dumps({"consumptions": sum_panel}),
         mimetype="application/json"
                     )
